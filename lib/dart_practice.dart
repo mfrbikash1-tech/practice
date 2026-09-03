291f35510a535abc9eefd5143e7b77f29eeead9f
mfrbikash1-tech <mfr.bikash1@gmail.com>
2026-09-03 11:26:41 +0600

    emeil added

--- lib/dart_practice.dart
+++ lib/dart_practice.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 void main(List<String> args) {
+  print("Enter Your Name: ");
   String? name =stdin.readLineSync();
   print("Your Name is $name");
   String age =stdin.readLineSync()!;
   print("Your Age is $age");
+  String email =stdin.readLineSync()!;
+  print("Your email is $email");
 }
