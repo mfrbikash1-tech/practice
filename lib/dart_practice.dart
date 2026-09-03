10d5b9ff3f3d26bea1801dd5e8fd7675c7f7832e
mfrbikash1-tech <mfr.bikash1@gmail.com>
2026-09-03 11:17:06 +0600

    Age Added

--- lib/dart_practice.dart
+++ lib/dart_practice.dart
@@ -3,4 +3,6 @@ import 'dart:io';
 void main(List<String> args) {
   String? name =stdin.readLineSync();
   print("Your Name is $name");
+  String age =stdin.readLineSync()!;
+  print("Your Age is $age");
 }
