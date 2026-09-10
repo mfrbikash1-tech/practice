f7752bc7d2153d660b7bf6ce65ae574491876f49
mfrbikash1-tech <mfr.bikash1@gmail.com>
2026-09-10 08:46:41 +0600

    Assignment - Module 3

--- lib/dart_practice.dart
+++ lib/dart_practice.dart
@@ -1,11 +1,57 @@
 import 'dart:io';
 
-void main(List<String> args) {
-  print("Enter Your Name: ");
-  String? name =stdin.readLineSync();
-  print("Your Name is $name");
-  String age =stdin.readLineSync()!;
-  print("Your Age is $age");
-  String email =stdin.readLineSync()!;
-  print("Your email is $email");
+class BankAccount{
+  String acName;
+  int accNum;
+  int _balance;
+
+  BankAccount (this.acName,this.accNum,this._balance){
+    print("Account Created");
+  }
+
+  int getBalance(){
+    return _balance;
+  }
+
+  int deposite(String tkdeposite){
+    int tkdeposite2 = int.parse(tkdeposite);
+    if(tkdeposite2 <= 0){
+      print('Invalid amount');
+    }else{
+      _balance = _balance + tkdeposite2; 
+      print('Deposite balance: $_balance');
+    }
+   return getBalance();
+  }
+
+  int withdraw(String tk1){
+    int tk1Int = int.parse(tk1);
+    if(tk1Int>=getBalance()){
+      print('Invalid amount');
+    }else{
+      _balance = _balance - tk1Int;
+      print('Remaining balance: ${getBalance()}');
+    }
+   return getBalance();
+  }
+   dynamic displayinfo() {
+    print('Account Name : $acName');
+    print('Account Number : $accNum');
+    print('Your Balance remaining : ${getBalance()}');
+   }
 }
+
+
+
+void main(List<String> args) {
+BankAccount x=BankAccount("Rahim", 1, 1000);
+BankAccount y=BankAccount("Karim", 2, 1500);
+x.displayinfo();
+y.displayinfo();
+print('Enter Your Deposite Amount:');
+x.deposite(stdin.readLineSync() ?? '0');
+print('Enter Your Withdrawl Amount:');
+x.withdraw(stdin.readLineSync() ?? '0');
+
+}
+  
